de4681be3e56482b506c402d98a78d3223ce4c11
slmnkhn6590 <slmnkhn.6590@gmail.com>
2025-09-09 14:34:24 +0200

    this is my first exercise of my programme software development

---
+++ ex01t01.py
@@ -0,0 +1,6 @@
+User_name = input("what is your name?")
+Amount_of_money = int(input("How much money do you have?"))
+Rate_of_interest = int(input("please enter the rate in percent"))
+
+#print all the the stored information
+print(f"Name: {User_name}\n"f"Balance: {Amount_of_money}\n"f"Interest rate: {Rate_of_interest}")
